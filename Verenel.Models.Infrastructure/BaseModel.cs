107217e4341da482ae9bb0389d11e18edd94fcf2
Oğuzhan Sarıçam <oguzhan.saricam@outlook.com.tr>
2021-02-27 11:45:44 +0300

    models infrastructure project

---
+++ Verenel.Models.Infrastructure/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Verenel.Models.Infrastructure
+{
+    public class BaseModel : IModel<Guid>
+    {
+        public Guid SiteId { get; set; }
+        public Guid Id { get; set; }
+        public int CreateUser { get; set; }
+        public DateTime CreateionDate { get; set; }
+        public bool? IsDeleted { get; set; }
+        public DateTime? DeletionDate { get; set; }
+    }
+}
---
+++ Verenel.Models.Infrastructure/IModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Verenel.Models.Infrastructure
+{
+    public interface IModel<TKey>
+    {
+        /// <summary>
+        /// Group site
+        /// </summary>
+        TKey SiteId { get; set; }
+
+        /// <summary>
+        /// Unique object
+        /// </summary>
+        TKey Id { get; set; }
+
+        int CreateUser { get; set; }
+        DateTime CreateionDate { get; set; }
+        bool? IsDeleted { get; set; }
+        DateTime? DeletionDate { get; set; }
+    }
+}
